920bc7a41ba7a7544018b308b167e95e58105534
JSH <blacklucky21@naver.com>
2019-08-22 16:36:58 +0900

    2019 08 22

---
+++ src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,31 @@
+package com.kh.spring.member.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.spring.member.model.dao.MemberDAO;
+import com.kh.spring.member.model.vo.Member;
+
+@Service("mService")
+public class MemberServiceImpl implements MemberService{
+
+	
+	@Autowired
+	private MemberDAO mDAO;
+	
+	
+	
+	@Override
+	public Member memberLogin(Member m) {
+		
+		
+		return mDAO.selectMember(m);
+		
+
+	}
+	
+
+	
+	
+
+}
